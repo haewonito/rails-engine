0a4fcd83b430195628a8883b1b49104899ef8519
Haewon Jeon <haewonito@gmail.com>
2021-12-06 22:37:24 -0700

    #3

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,6 +1,17 @@
 class Api::V1::MerchantsController < ApplicationController
 
   def index
+    render json: Merchant.all
+  end
+
+  def show
+    render json: Merchant.find(params[:id])
+  end
+
+  private
+
+  def merchant_params
+    params.require(:merchant).permit(:name, :id)
   end
 
 end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -8,6 +8,25 @@ describe "Merchants API" do
 
     expect(response).to be_successful
 
-    merchants = JSON.parse(response.body)
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants.count).to be 3
+
+    merchants.each do |merchant|
+      expect(merchant[:id]).to be_an(Integer)
+      expect(merchant[:name]).to be_a(String)
+    end
+  end
+
+  it "sends a merchant by the id" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(merchant[:name]).to be_a(String)
   end
 end
