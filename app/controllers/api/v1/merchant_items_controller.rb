f81410ca9e8f00c28fe682d0de972746f55f01a5
Haewon Jeon <haewonito@gmail.com>
2021-12-09 10:57:19 -0700

    2 and 3 complete. 4 half complete

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -2,7 +2,16 @@ class Api::V1::MerchantItemsController < ApplicationController
 
   def index
     merchant = Merchant.find(params[:merchant_id])
-    render json: ItemSerializer.new(merchant.items.all)
+    items = merchant.items
+    render json: ItemSerializer.new(items)
+# require "pry"; binding.pry
+#     if Merchant.exists?(params[:merchant_id])
+#       merchant = Merchant.find(params[:merchant_id])
+#       items = merchant.items
+#       render json: ItemSerializer.new(items)
+#     else
+#       render json: {errors: {details: "A merchant with this id does not exist."}}, status: 404
+#     end
   end
 
 end
