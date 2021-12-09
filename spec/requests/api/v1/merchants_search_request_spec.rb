9b252235bd302ce1e29e7ee9468acf58b33a956b
Haewon Jeon <haewonito@gmail.com>
2021-12-09 14:12:39 -0700

    not sure what this is

---
+++ spec/requests/api/v1/merchants_search_request_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe "Merchants_search API" do
+  before(:each) do
+    @merchant1 = Merchant.create(name: "Turing")
+    @merchant2 = Merchant.create(name: "Titanium Rings")
+    @merchant3 = Merchant.create(name: "Rings That You Love")
+  end
+
+  it "happy path: sends the first search result - first in alphabetical order" do
+
+    get "/api/v1/merchants/find?name=Ring"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchant[:data]).to be_a(Hash)
+    expect(merchant[:data][:id]).to eq(@merchant3.id.to_s)
+    expect(merchant[:data][:attributes][:name]).to eq(@merchant3.name)
+
+  end
+
+  it "happy path: sends the first search result: case insensitive" do
+
+    get "/api/v1/merchants/find?name=ti"
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(merchant[:data][:id]).to eq(@merchant2.id.to_s)
+    expect(merchant[:data][:attributes][:name]).to eq(@merchant2.name)
+  end
+
+end
